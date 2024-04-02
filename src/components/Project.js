d4768e0b4ae7ae9a9a7a31df5640a3736cea18c1
Jameson Brown <jamesonbrown@neolivity.com>
2024-04-01 18:34:35 -0600

    Added missing project and link to Github

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,10 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faLink } from "@fortawesome/free-solid-svg-icons"
 
 import GillulyScreenshot from "../assets/gilluly_screenshot.png"
 import HackernoonScreenshot from "../assets/hackernoon_screenshot.png"
+import SlideDeckScreenshot from "../assets/slidedeck_screenshot.jpeg"
 import HackernoonRedux from "../assets/hackernoon_redux.jpeg"
 import DesignerPortfolio from "../assets/designer_portfolio.jpeg"
 import LinkArrow from "../assets/link_arrow.svg"
@@ -16,22 +19,9 @@ export const Project = (props) => {
       return HackernoonRedux
     } else if (props.title === "Designer Portfolio") {
       return DesignerPortfolio
+    } else if (props.title === "MARP Cheatsheet - Article") {
+      return SlideDeckScreenshot
     }
-  }
-  const getProjectLink = () => {
-    if (props.title === "Gilluly Planner") {
-      return "https://gilluly.jamesonb.com"
-    } else if (props.title === "Stripe API - Article") {
-      return "https://hackernoon.com/stripe-api-reactjs-and-express-bc446bf08301"
-    } else if (props.title === "Build Redux - Article") {
-      return "https://medium.com/@jamesonbrown/build-a-simple-redux-from-scratch-63908db092b9"
-    } else if (props.title === "Designer Portfolio") {
-      return "https://barelyfiction.design"
-    }
-  }
-  const data = {
-    src: getProjectThumbail(),
-    link: getProjectLink(),
   }
 
   return (
@@ -40,17 +30,38 @@ export const Project = (props) => {
         `rounded-[18px] bg-[#2A2A2A] cursor-pointer max-w-[390px] w-full sm:w-[calc(50%-30px)] ` +
         props.containerStyles
       }
-      onClick={() => window.open(data.link, "_blank")}
+      onClick={() => window.open(props.url, "_blank")}
     >
-      <img alt="" className="max-h-[235px] mx-auto mb-auto" src={data.src} />
+      <img
+        alt=""
+        className="max-h-[235px] mx-auto mb-auto"
+        src={getProjectThumbail()}
+      />
       <div className="flex justify-between items-center pb-[14px] pt-[12px] px-[23px]">
-        <div>
+        <div className="flex flex-col">
           <p className="text-[#C5C5C5] text-[10px] font-[800] uppercase text-left font-Poppins">
             Click here to visit
           </p>
           <h3 className="text-[#FFFFFF] text-[19px] font-[800] text-left uppercase font-Poppins">
             {props.title}
           </h3>
+          {props.github && (
+            <span>
+              <button
+                className="text-[#FFFFFF] text-sm flex items-center px-[6px] my-[4px] italic"
+                onClick={(e) => {
+                  e.stopPropagation()
+                  window.open(props.github, "_blank")
+                }}
+              >
+                <FontAwesomeIcon
+                  className="h-[14px] w-[14px] mr-[4px]"
+                  icon={faLink}
+                />{" "}
+                Github
+              </button>
+            </span>
+          )}
         </div>
         <img alt="link arrow" src={LinkArrow} />
       </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,17 +9,31 @@ export const Projects = () => {
         Projects
       </h2>
       <div className="flex flex-col sm:flex-row max-w-[650px] mx-auto flex-wrap">
-        <Project title="Gilluly Planner" containerStyles="sm:mr-[30px]" />
         <Project
-          title="Build Redux - Article"
-          containerStyles="mt-[30px] sm:mt-0"
-        />
-        <Project
-          title="Stripe API - Article"
-          containerStyles="mt-[30px] sm:mr-[30px]"
+          title="Gilluly Planner"
+          github="https://github.com/Jameson13B/gilluly"
+          url="https://gilluly.jamesonb.com"
+          containerStyles="sm:mr-[30px]"
         />
         <Project
           title="Designer Portfolio"
+          github="https://github.com/Christopher-Brown1/portfolio"
+          url="https://barelyfiction.design"
+          containerStyles="mt-[30px] sm:mt-0"
+        />
+        <Project
+          title="Build Redux - Article"
+          url="https://medium.com/@jamesonbrown/build-a-simple-redux-from-scratch-63908db092b9"
+          containerStyles="mt-[30px] sm:mr-[30px]"
+        />
+        <Project
+          title="MARP Cheatsheet - Article"
+          url="https://medium.com/@jamesonbrown/how-developers-create-slide-decks-ef9013aab420"
+          containerStyles="mt-[30px] sm:mt-[30px]"
+        />
+        <Project
+          title="Stripe API - Article"
+          url="https://hackernoon.com/stripe-api-reactjs-and-express-bc446bf08301"
           containerStyles="mt-[30px] sm:mr-[30px]"
         />
       </div>
